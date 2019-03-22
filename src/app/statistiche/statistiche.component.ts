6935000cf52c4faf4e811f067b28999f6c89c5c7
Luciano Fusco <luciano.fusco@nttdata.com>
2019-03-22 17:09:44 +0100

    Esempio per Riempire il grafico
    Esempio per Riempire il grafico

--- src/app/statistiche/statistiche.component.ts
+++ src/app/statistiche/statistiche.component.ts
@@ -223,6 +223,28 @@ export class StatisticheComponent implements OnInit {
 
   private buildGraphicData(valori: any[], scelte: any[], scelteTotali: number): void {
 
+    // Logica Per Riempire con i miei dati
+
+    // Esempio
+    this.barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+
+    this.barChartData = [
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+    ];
+
+    const clone = JSON.parse(JSON.stringify(this.barChartData));
+    const data = [
+      Math.round(Math.random() * 100),
+      59,
+      80,
+      (Math.random() * 100),
+      56,
+      (Math.random() * 100),
+      40];
+    clone[0].data = data;
+    this.barChartData = clone;
+    // Esempio
 
   }
 
