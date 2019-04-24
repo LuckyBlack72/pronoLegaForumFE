c39de32185cc9797fe82a910084d8fdbde99f653
Luciano Fusco <luciano.fusco@nttdata.com>
2019-04-24 16:27:57 +0200

    spinner per il caricamento delle pagine
    spinner per il caricamento delle pagine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Event, Router, NavigationStart, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pronostici Lega Forum';
+
+  // per caricare lo spinner sulla navigazione tra pagine
+  showLoadingIndicator = true;
+  constructor ( private router: Router) {
+    this.router.events.subscribe((routerEvent: Event) => {
+      if ( routerEvent instanceof NavigationStart ) {
+        this.showLoadingIndicator = true;
+      }
+      if ( routerEvent instanceof NavigationEnd ) {
+        this.showLoadingIndicator = false;
+      }
+    });
+  }
+  // per caricare lo spinner sulla navigazione tra pagine
+
 }
